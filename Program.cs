02ccbf81c0971fbbb577d35868f630d4aa8a438f
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2022-01-24 11:59:05 -0500

    phase 6 completed, conditionals added for low/high guesses

--- Program.cs
+++ Program.cs
@@ -3,18 +3,26 @@
     static void Main(string[] args)
     {
         Random random = new Random();
-        int secretNumber = random.Next(1,100);
+        int secretNumber = random.Next(1, 100);
         int guessCount = 0;
         int input;
 
 
         while (guessCount < 4)
         {
-            Console.WriteLine($"Random number to guess is: {secretNumber}");
+            // Console.WriteLine($"Random number to guess is: {secretNumber}");
             Console.WriteLine("Guess the secret number:");
             input = Convert.ToInt32(Console.ReadLine());
 
-            if (input == secretNumber)
+            if (input < secretNumber)
+            {
+                Console.WriteLine("\nToo Low");
+            }
+            else if (input > secretNumber)
+            {
+                Console.WriteLine("\nToo High");
+            }
+            else if (input == secretNumber)
             {
                 Console.WriteLine("\nCORRECT!");
                 break;
@@ -24,10 +32,11 @@
                 Console.WriteLine("Sorry, incorrect guess!");
                 Console.WriteLine($"\nPrevious guess ({input})");
                 guessCount++;
-                Console.WriteLine($"Number of guesses left ({4-guessCount})");
+                Console.WriteLine($"Number of guesses left ({4 - guessCount})");
 
             }
 
         }
     }
+
 }
